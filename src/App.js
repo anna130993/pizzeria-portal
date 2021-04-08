45213580bddced85302513f6e0950d3f4fd74dcb
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-04-09 00:46:06 +0200

    Add MaterialUI and Appbar, Toolbar, new theme

--- src/App.js
+++ src/App.js
@@ -13,26 +13,39 @@ import Waiter from './components/views/Waiter/Waiter';
 import Order from './components/views/Order/Order';
 import NewOrder from './components/views/NewOrder/NewOrder';
 import Kitchen from './components/views/Kitchen/Kitchen';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
-function App() {
-  return (
-    <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`/`} component={Dashboard} />
-          <Route exact path={`/login`} component={Login} />
-          <Route exact path={`/tables`} component={Tables} />
-          <Route exact path={`/tables/booking/new`} component={NewBooking} />
-          <Route exact path={`/tables/booking/:id`} component={Booking} />
-          <Route exact path={`/tables/events/new`} component={NewEvent} />
-          <Route exact path={`/tables/events/:id`} component={Event} />
-          <Route exact path={`/waiter`} component={Waiter} />
-          <Route exact path={`/waiter/order/new`} component={NewOrder} />
-          <Route exact path={`/waiter/order/:id`} component={Order} />
-          <Route exact path={`/kitchen`} component={Kitchen} />
-        </Switch>
-      </MainLayout>
-    </BrowserRouter>
-  );
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: {main: '#2B4C6F'},
+    //*secondary: {main: '#11cb5f'},
+  },
+});
+
+const App = () => (
+  <BrowserRouter basename={'/panel'}>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <MainLayout>
+          <Switch>
+            <Route exact path={`/`} component={Dashboard} />
+            <Route exact path={`/login`} component={Login} />
+            <Route exact path={`/tables`} component={Tables} />
+            <Route exact path={`/tables/booking/new`} component={NewBooking} />
+            <Route exact path={`/tables/booking/:id`} component={Booking} />
+            <Route exact path={`/tables/events/new`} component={NewEvent} />
+            <Route exact path={`/tables/events/:id`} component={Event} />
+            <Route exact path={`/waiter`} component={Waiter} />
+            <Route exact path={`/waiter/order/new`} component={NewOrder} />
+            <Route exact path={`/waiter/order/:id`} component={Order} />
+            <Route exact path={`/kitchen`} component={Kitchen} />
+          </Switch>
+        </MainLayout>
+      </ThemeProvider>
+    </StylesProvider>
+  </BrowserRouter>
+);
+
 export default App;
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-
+import Button from '@material-ui/core/Button';
+import styles from './PageNav.module.scss';
 
 const PageNav = () => (
-  <div>
-    <NavLink exact to={`/`} activeClassName='active'>Dashboard</NavLink>
-    <NavLink to={`/login`} activeClassName='active'>Login</NavLink>
-    <NavLink to={`/tables`} activeClassName='active'>Tables</NavLink>
-    <NavLink to={`/waiter`} activeClassName='active'>Waiter</NavLink>
-    <NavLink to={`/kitchen`} activeClassName='active'>Kitchen</NavLink>
-  </div>
+  <nav className={styles.component}>
+    <Button className={styles.link} component={NavLink} exact to={`/`} activeClassName='active'>Dashboard</Button>
+    <Button className={styles.link} component={NavLink} to={`/login`} activeClassName='active'>Login</Button>
+    <Button className={styles.link} component={NavLink} to={`/tables`} activeClassName='active'>Tables</Button>
+    <Button className={styles.link} component={NavLink} to={`/waiter`} activeClassName='active'>Waiter</Button>
+    <Button className={styles.link} component={NavLink} to={`/kitchen`} activeClassName='active'>Kitchen</Button>
+  </nav>
 );
 
 export default PageNav;
